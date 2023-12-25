6d454178252af4abba1618c9096423b41cea0620
vinita3027 <vinitanwar8959@gamil.com>
2023-12-25 17:05:45 +0530

    files adeded

---
+++ README.md
@@ -0,0 +1,45 @@
+# <p align="center"> The Wall of Projects</p>
+
+
+[![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/ellerbrock/open-source-badges/)
+![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat)
+![Visitors](https://api.visitorbadge.io/api/visitors?path=MrAshwin2142%2FThe-Wall-of-Projects%20&countColor=%23263759&style=flat)
+![GitHub forks](https://img.shields.io/github/forks/MrAshwin2142/The-Wall-of-Projects)
+![GitHub Repo stars](https://img.shields.io/github/stars/MrAshwin2142/The-Wall-of-Projects)
+![GitHub contributors](https://img.shields.io/github/contributors/MrAshwin2142/The-Wall-of-Projects)
+![GitHub last commit](https://img.shields.io/github/last-commit/MrAshwin2142/The-Wall-of-Projects)
+![GitHub repo size](https://img.shields.io/github/repo-size/MrAshwin2142/The-Wall-of-Projects)
+
+![Github](https://img.shields.io/github/license/MrAshwin2142/The-Wall-of-Projects)
+![GitHub issues](https://img.shields.io/github/issues/MrAshwin2142/The-Wall-of-Projects)
+![GitHub closed issues](https://img.shields.io/github/issues-closed-raw/MrAshwin2142/The-Wall-of-Projects)
+![GitHub pull requests](https://img.shields.io/github/issues-pr/MrAshwin2142/The-Wall-of-Projects)
+![GitHub closed pull requests](https://img.shields.io/github/issues-pr-closed/MrAshwin2142/The-Wall-of-Projects)
+
+
+```code
+⭐  If you find our project valuable, please consider giving it a star. ⭐
+```
+# The Wall of Projects
+
+The Wall of Projects is a collaborative web platform designed to showcase a diverse collection of projects contributed by developers worldwide. It serves as a hub for sharing, exploring, and contributing to various projects across different domains.
+To explore projects, simply visit [The Wall of Projects](https://the-wall-of-projects.netlify.app/).
+## Features
+
+- **Project Showcase:** Explore a wide range of projects spanning web development, mobile apps, machine learning, and more.
+- **Contributor Recognition:** Highlighting contributors and their projects fosters a supportive community.
+- **User-friendly Interface:** Easy navigation for exploring project details and connections with contributors.
+- **Open Contribution:** Welcoming developers of all skill levels to contribute their projects.
+
+## How to Contribute
+
+There are two ways to add your project to The Wall of Projects:
+
+1. **Google Form Submission:** Access the [Contribute](https://forms.gle/6eKToerpPrGK2vhZA) and fill in the details about your project.
+2. **Manual Addition via `projects.js` File:** Follow the instructions in [`contributors.md`](https://github.com/MrAshwin2142/The-Wall-of-Projects/blob/main/CONTRIBUTE.md) to add your project directly into the `projects.js` file.
+
+## Benefits
+
+- **Learning Resource:** Gain insights into various projects and technologies, offering a learning platform for developers.
+- **Collaborative Environment:** Encourages collaboration, networking, and knowledge-sharing among contributors.
+- **Inclusive Space:** Open to beginners and experienced developers, providing a platform to showcase their work.
---
+++ script.js
@@ -0,0 +1,153 @@
+// Define functions outside of the DOMContentLoaded event
+function displayProjectDetails(project, elements) {
+  // Destructuring elements for ease of use
+  const {
+    projectName,
+    projectDescription,
+    contributor,
+    usedTech,
+    codeLink,
+    liveLink,
+    liveLinkError,
+    projectImage,
+    detailSection,
+  } = elements;
+
+  const shouldBeHidden =
+    !detailSection.classList.contains("hidden") &&
+    projectName.textContent === project.name;
+  detailSection.classList.toggle("hidden", shouldBeHidden);
+
+  // Set project details
+  projectName.textContent = project.name;
+  projectDescription.textContent = project.description;
+  contributor.textContent = project.contributorName;
+  usedTech.textContent = project.usedTech;
+  codeLink.href = project.codeLink;
+
+  // Handle live link
+  liveLink.href = project.liveLink || "javascript:void(0);";
+  if (!project.liveLink) {
+    liveLinkError.classList.remove("hidden");
+    liveLink.style.pointerEvents = "none";
+  } else {
+    liveLinkError.classList.add("hidden");
+    liveLink.style.pointerEvents = "";
+  }
+
+  // Set project image
+  projectImage.src = project.image || "./images/default-image.jpg";
+}
+
+// Wait until the DOM is fully loaded before running the script
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = {
+    projectName: document.getElementById("projectName"),
+    projectImage: document.getElementById("projectImage"),
+    projectDescription: document.getElementById("projectDescription"),
+    contributor: document.getElementById("contributor"),
+    codeLink: document.getElementById("codeLink"),
+    liveLink: document.getElementById("liveLink"),
+    detailSection: document.getElementById("detailSection"),
+    usedTech: document.getElementById("usedTech"),
+    contributorTable: document.getElementById("contributorTable"),
+    liveLinkError: document.getElementById("liveLinkError"),
+    projectCount: document.getElementById("projectCount"),
+  };
+
+  const starContributor = {};
+  const projectList = document.getElementById("projectList");
+  let currentClickedButton = null;
+
+  projects.forEach((project) => {
+    const projectButton = document.createElement("button");
+    projectButton.textContent = project.name;
+    starContributor[project.contributorName] =
+      (starContributor[project.contributorName] || 0) + 1;
+    projectButton.className = "btn";
+    projectButton.setAttribute("data-tech", project.usedTech.toLowerCase());
+    projectButton.setAttribute("data-name", project.name.toLowerCase());
+    projectButton.setAttribute(
+      "data-contributor",
+      project.contributorName.toLowerCase()
+    );
+    projectButton.title = project.usedTech;
+
+    projectList.appendChild(projectButton);
+  });
+
+  searchBox.addEventListener("input", (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+
+    const buttons = projectList.getElementsByTagName("button");
+    for (const button of buttons) {
+      const tech = button.getAttribute("data-tech") || "";
+      const name = button.getAttribute("data-name") || "";
+      const contributor = button.getAttribute("data-contributor") || "";
+
+      if (
+        tech.includes(searchTerm) ||
+        name.includes(searchTerm) ||
+        contributor.includes(searchTerm)
+      ) {
+        button.style.display = ""; // Show the button
+      } else {
+        button.style.display = "none"; // Hide the button
+      }
+    }
+  });
+
+  // Event delegation for project buttons
+  projectList.addEventListener("click", (event) => {
+    const projectButton = event.target;
+    if (projectButton.tagName === "BUTTON") {
+      const project = projects.find(
+        (p) => p.name === projectButton.textContent
+      );
+      if (project) {
+        handleButtonClick(projectButton, project);
+      }
+    }
+  });
+
+  function handleButtonClick(projectButton, project) {
+    const isCurrentButton = currentClickedButton === projectButton;
+    if (currentClickedButton) {
+      currentClickedButton.classList.remove("active");
+    }
+
+    displayProjectDetails(project, elements);
+
+    if (!elements.detailSection.classList.contains("hidden")) {
+      if (!isCurrentButton) {
+        projectButton.classList.add("active");
+        currentClickedButton = projectButton;
+      } else {
+        currentClickedButton = null;
+      }
+    } else {
+      currentClickedButton = null;
+    }
+  }
+  const sortedStarContributors = Object.entries(starContributor).sort(
+    (a, b) => b[1] - a[1]
+  );
+
+  sortedStarContributors.forEach(([contributorName, projectsCount]) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `<td>${contributorName}</td><td>${projectsCount}</td>`;
+    elements.contributorTable.appendChild(row);
+  });
+  projectCount.textContent = `Total Projects Hosted: ${projects.length}`;
+});
+
+function handleMode() {
+  const element = document.getElementById("modeButton");
+  element.classList.toggle("lightIcon");
+  document.body.classList.toggle("dark-Theme");
+}
+
+// Handling Menu toggle on mobile screen
+function toggleSlideInMenu() {
+  document.getElementById("slideInMenu").classList.toggle("hidden");
+}
